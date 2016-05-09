863512b7ae7f20a6fe542f1e81f4e6ba32b70f24
Finn Poppinga <finno.ger@gmail.com>
2016-05-09 12:51:43 +0200

    working ListOrders

--- amazon/api.ts
+++ amazon/api.ts
@@ -1,4 +1,7 @@
+import _ = require('underscore');
 import moment = require('moment');
+import crypto = require('crypto');
+import request = require('request');
 
 module Amazon {
     export interface ListOrdersRequest {
@@ -36,10 +39,106 @@ module Amazon {
             this.sellerId = process.env.AMAZON_MERCHANT_ID;
             this.awsAccountId = process.env.AMAZON_ACCESS_KEY_ID;
             this.secretKey = process.env.AMAZON_SECRET_ACCESS_KEY;
+
+            this.endpoint = '/Orders/2013-09-01'
+        }
+
+        private getStringToSign(input : {[key : string] : string}) : string {
+            var keys = [];
+            for(var k in input) keys.push(k);
+            var str = '';
+
+            keys = keys.sort();
+
+            for(var i=0;i<keys.length;i++) {
+                if(i != 0) str += '&';
+                str += keys[i] + '=' + input[keys[i]];
+            }
+
+            return 'POST\n' +
+                'mws.amazonservices.de\n' +
+                this.endpoint + '\n' +
+                str;
+        }
+
+        private hexStrToBase64(input:string):string {
+            let b64pad = '=';
+
+            var tab = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+            var hex = "0123456789abcdef";
+            var output = "";
+            var len = input.length;
+            for (var i = 0; i < len; i += 6) {
+                var c1 = hex.indexOf(input.charAt(i)) << 4;
+                c1 += hex.indexOf(input.charAt(i + 1));
+                var c2 = (i + 2) < len ? hex.indexOf(input.charAt(i + 2)) << 4 : 0;
+                c2 += (i + 3) < len ? hex.indexOf(input.charAt(i + 3)) : 0;
+                var c3 = (i + 4) < len ? hex.indexOf(input.charAt(i + 4)) << 4 : 0;
+                c3 += (i + 5) < len ? hex.indexOf(input.charAt(i + 5)) : 0;
+
+                var triplet = (c1 << 16)
+                    | c2 << 8
+                    | c3;
+                for (var j = 0; j < 4; j++) {
+                    //if(i * 16 + j * 12 > input.length * 16) output += b64pad;
+                    if ((i / 2) * 8 + j * 6 > (input.length / 2) * 8) output += b64pad;
+                    else output += tab.charAt((triplet >>> 6 * (3 - j)) & 0x3F);
+                }
+            }
+
+            return output;
+        }
+
+        public createSignature(input : {[key : string] : string}, secret : string) : string {
+            let strToSign : string = this.getStringToSign(input);
+            let hmac : string = crypto.createHmac('sha256', secret).update(strToSign).digest('hex');
+
+            let b64hmac : string = this.hexStrToBase64(hmac);
+
+            //padding to string length multiple of 4
+            var j = b64hmac.length % 4;
+            for (var i = 0; i < j; i++) {
+                b64hmac += '=';
+            }
+
+            return b64hmac;
         }
 
         public listOrders(options : ListOrdersRequest, callback : (err? : any, result? : any) => void) {
-            
+            var parameters : {[key : string] : string} = {};
+            // parameters to be signed
+            parameters['Action'] = 'ListOrders';
+            parameters['SellerId'] = urlEncode(this.sellerId);
+            parameters['CreatedAfter'] = urlEncode(options.CreatedAfter.toISOString());
+            var count = 0;
+            _.each(options['MarketplaceId.Id'], (id : string) => {
+                parameters['MarketplaceId.Id.' + ++count] = urlEncode(id);
+            });
+            parameters['AWSAccessKeyId'] = this.awsAccountId;
+            parameters['SignatureMethod'] = 'HmacSHA256';
+            parameters['SignatureVersion'] = '2';
+
+            parameters['Timestamp'] = urlEncode(moment().toISOString());
+            parameters['Version'] = '2013-09-01';
+
+            // signature
+            parameters['Signature'] = urlEncode(this.createSignature(parameters, this.secretKey));
+
+            var userAgent : string = 'pptest/1.0 (Language=Javascript)';
+            var contentType : string = 'text/xml';
+
+            var queryString : string = _.map(parameters, function(value, key) {
+                return key + '=' + value;
+            }).join('&');
+
+            console.log('queryString', queryString);
+
+            request.post('https://mws.amazonservices.de' + this.endpoint + '?' + queryString, {
+                headers : {
+                    'x-amazon-user-agent' : userAgent,
+                    'Content-Type' : contentType
+                }
+            }, callback);
         }
     }
 }
--- spec/amazon.spec.ts
+++ spec/amazon.spec.ts
@@ -8,11 +8,13 @@ describe('Amazon', function() {
 
     it('can list orders.', function(done) {
         var req : Amazon.ListOrdersRequest = {
-            CreatedAfter: moment(),
+            CreatedAfter: moment().subtract(24, 'hours'),
             'MarketplaceId.Id' : ['A1PA6795UKMFR9']
         };
 
         amazon.listOrders(req, function(err, result) {
+            console.log('error', err);
+            console.log('result', result.body);
             expect(err).toBeFalsy();
             done();
         })
