566bee70f9eb0963814c9f993efbe4a3c2dcb278
Johannes Bornholdt <yoyobo@gmail.com>
2016-05-19 15:14:14 +0200

    added changed files

--- src/typings/browser.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="browser/ambient/iconv/index.d.ts" />
-/// <reference path="browser/ambient/jasmine/index.d.ts" />
-/// <reference path="browser/ambient/node/index.d.ts" />
-/// <reference path="browser/ambient/underscore/index.d.ts" />
-/// <reference path="browser/ambient/xml2js/index.d.ts" />
-/// <reference path="browser/definitions/moment/index.d.ts" />
-/// <reference path="browser/definitions/request/index.d.ts" />
