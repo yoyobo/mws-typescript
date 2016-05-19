a791a98190dbb0573b75fc18827f5b1577c61d78
Johannes Bornholdt <yoyobo@gmail.com>
2016-05-19 16:01:14 +0200

    changed bundling again

--- src/amazon/api.ts
+++ src/amazon/api.ts
@@ -6,33 +6,29 @@ import Orders = require('./orders');
 import Reports = require('./reports');
 import Feeds = require('./feeds');
 
-module Amazon {
-    export class MWS {
-        public credentials: AmazonTypes.Credentials;
-        constructor() {
-            this.credentials = {
-                sellerId: process.env.AMAZON_MERCHANT_ID,
-                awsAccountId: process.env.AMAZON_ACCESS_KEY_ID,
-                secretKey: process.env.AMAZON_SECRET_ACCESS_KEY,
-                host: 'mws.amazonservices.de'
-            };
+class MWS {
+    public credentials: AmazonTypes.Credentials;
+    constructor() {
+        this.credentials = {
+            sellerId: process.env.AMAZON_MERCHANT_ID,
+            awsAccountId: process.env.AMAZON_ACCESS_KEY_ID,
+            secretKey: process.env.AMAZON_SECRET_ACCESS_KEY,
+            host: 'mws.amazonservices.de'
+        };
 
-            this.Orders = new Orders.Orders(this.credentials);
-            this.Reports = new Reports.Reports(this.credentials);
-            this.Feeds = new Feeds.Feeds(this.credentials);
-        }
-
-        public Orders: Orders.Orders;
-
-        public Reports: Reports.Reports;
-
-        public Feeds: Feeds.Feeds;
+        this.Orders = new Orders.Orders(this.credentials);
+        this.Reports = new Reports.Reports(this.credentials);
+        this.Feeds = new Feeds.Feeds(this.credentials);
     }
 
+    public Orders: Orders.Orders;
 
+    public Reports: Reports.Reports;
+
+    public Feeds: Feeds.Feeds;
 }
 
 export = {
-    Amazon : Amazon,
+    MWS : MWS,
     AmazonTypes : AmazonTypes
 };
